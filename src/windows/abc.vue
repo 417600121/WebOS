1a3744c879e78237ab0ad53b5949206ecbeb61f5
LEI-SHI <417600121@qq.com>
2022-07-20 13:50:12 +0800

    video loop

--- src/windows/abc.vue
+++ src/windows/abc.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <video src="./assets/video/82363459_nb2-1-80.mp4" autoplay controls="true" style="width: 100%;height: 280px;transform: translateY(-8px);" muted="true"></video>
+        <video src="./assets/video/82363459_nb2-1-80.mp4" loop autoplay controls="true" style="width: 100%;height: 280px;transform: translateY(-8px);" muted="true"></video>
     </div>
 </template>
 
